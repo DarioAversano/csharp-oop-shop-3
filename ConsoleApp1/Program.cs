97fd31a428f92514f705f28c7ec407d70e3cf410
darioaversano <risusveloce@gmail.com>
2023-04-28 11:48:49 +0200

    MILESTONE 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,21 +28,11 @@ float numero = 0f;
 Console.WriteLine("I prodotti attualmente generati sono: " + Product.GetTotalGeneratedProducts());
 
 Product LavatriceSamsung = new Product("Lavatrice Samsung SE4000", "Lavatrice ultra performante in classe A", 999.99f, "elettrodomestico");
-Water AcquaSantAnna = new Water("Acqua", "Frizzante", 2f, "Alimentari", "Sant'Anna", 2.5f, 2f, 12f, "Vinadio");
+//Water AcquaSantAnna = new Water("Acqua", "Frizzante", 2f, "Alimentari", "Sant'Anna", 2.5f, 2f, 12f, "Vinadio");
 Fruit Limone = new Fruit("Limone", "Limone grande", 0.5f, "Alimentari", "Sicilia", "Agrume");
 Fruit Mela1 = new Fruit("Mela verde", "Mela rosso", 0.2f, "mela", "Roma", "frutta");
 Fruit Mela2 = new Fruit("Mela gialla", "Mela gialla", 0.2f, "mela", "Roma", "frutta");
 
-try
-{
-//    Water AcquaSantAnna = new Water("Acqua", "Frizzante", 2f, "Alimentari", "Sant'Anna", 2.5f, 2f, 12f, "Vinadio");
-    Console.WriteLine(AcquaSantAnna);
-    //Console.WriteLine(AcquaSantAnna.ToString); Chiamato in automatico da console writeline quando c'è un oggetto.
-}
-catch (Exception exception)
-{
-	Console.WriteLine("Errore Errore Errore" + exception.Message);
-}
 
 Fruit MelaSacchetto1 = new Fruit("Mela verde", "Mela verde", 0.2f, "mela", "Roma", "frutta");
 Fruit MelaSacchetto2 = new Fruit("Mela verde", "Mela verde", 0.2f, "mela", "Roma", "frutta");
@@ -108,7 +98,16 @@ di carrelli che eventualmente gli utenti sono andati a prendere (ossia generati 
 
 
 // COSI IL PROGRAMMA NON SI BLOCCA E VA NEL PUNTO CATCH *1*1*1*1*1*1*1
-
+try
+{
+    Water AcquaSantAnna = new Water("Acqua", "Frizzante", 2f, "Alimentari", "Sant'Anna", 2.5f, 2f, 12f, "Vinadio");
+    Console.WriteLine(AcquaSantAnna);
+    //Console.WriteLine(AcquaSantAnna.ToString); Chiamato in automatico da console writeline quando c'è un oggetto.
+}
+catch (Exception exception)
+{
+	Console.WriteLine("Errore Errore Errore" + exception.Message);
+}
 
 
 Console.WriteLine("finito il programma");
